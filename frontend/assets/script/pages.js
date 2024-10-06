94b730efadafed565792f026242c195ff765fd1c
Mtho <mthontanzi99@gmail.com>
2024-10-06 05:04:08 +0200

    Update frontend, index and pages.js view is near done

--- frontend/assets/script/pages.js
+++ frontend/assets/script/pages.js
@@ -16,23 +16,22 @@ function getRouteContent(route) {
     switch (route) {
         case 'review':
             return `
-                <h1 class="text-3xl">Review</h1>
-                <form class="flex max-w-96 flex-col border-solid rounded-lg">
-                    <div class="flex justify-between py-3">
-                        <label for="userArea">Area</label>
-                        <button type="button" id="getLocationBtn" class="bg-emerald-500 h-12 px-6 rounded-lg font-semibold text-white">Get Location</button>
-                        <input type="text" name="latitude" id="latitude" class="border-solid border-2 rounded-lg border-slate-950 mx-2">
-                        <input type="text" name="longitude" id="longitude" class="border-solid border-2 rounded-lg border-slate-950 mx-2">
+                <h1 class="text-3xl mb-6">Review</h1>
+                <form class="flex justify-self-center self-center w-5/6 max-w-96 flex-col justify-center content-center border-solid rounded-lg">
+                    <div class="flex flex-col justify-between">
+                        <button type="button" id="getLocationBtn" class="bg-emerald-500 h-6 px-6 rounded-lg font-semibold text-white text-sm mb-2">Get Location</button>
+                        <input type="text" name="latitude" id="latitude" class="border-solid border-2 rounded-lg border-slate-950 mb-2">
+                        <input type="text" name="longitude" id="longitude" class="border-solid border-2 rounded-lg border-slate-950 mb-4">
                     </div>
-                    <select name="reviewNature" id="reviewNatureSelect" class="py-3">
+                    <select name="reviewNature" id="reviewNatureSelect" class="py-3 rounded-lg my-2">
                         <option value="" selected>Select nature of review</option>
                         <option value="compliment" id="reviewNature">Compliment</option>
                         <option value="complain" id="reviewNature">Complain</option>
                     </select>
                     
                     <div class="py-3">
-                        <label for="userReview">Write your review here.</label>
-                        <textarea name="userReview" id="userReview" maxlength="240" placeholder="Write your review here. Please limit to 240 characters" class="w-full h-20"></textarea>
+                        <label for="userReview">Write your review.</label>
+                        <textarea name="userReview" id="userReview" maxlength="240" placeholder="Write your review here. Please limit to 240 characters" class="w-full h-20 rounded-b-lg"></textarea>
                     </div>
                     <button type="submit" class="bg-emerald-500 rounded-lg p-2 text-base font-semibold text-white">Submit Review</button>
                 </form>
@@ -41,14 +40,13 @@ function getRouteContent(route) {
             return `
                 <h1 class="text-3xl">Concern</h1>
                 <p>Hello username, let us know of any concerns troubling your area. Please make your concern as descriptive as you possibly can and also make sure an accurate location</p>
-                <form class="flex max-w-96 flex-col border-solid rounded-lg">
-                    <div class="flex justify-between py-3">
-                        <label for="userArea">Area</label>
-                        <button type="button" id="getLocationBtn" class="bg-emerald-500 h-12 px-6 rounded-lg font-semibold text-white">Get Location</button>
-                        <input type="text" name="latitude" id="latitude" class="border-solid border-2 rounded-lg border-slate-950">
-                        <input type="text" name="longitude" id="longitude" class="border-solid border-2 rounded-lg border-slate-950">
+                <form class="flex justify-self-center self-center w-5/6 max-w-96 flex-col justify-center content-center border-solid rounded-lg">
+                    <div class="flex flex-col justify-between">
+                        <button type="button" id="getLocationBtn" class="bg-emerald-500 h-6 px-6 rounded-lg font-semibold text-white text-sm mb-2">Get Location</button>
+                        <input type="text" name="latitude" id="latitude" class="border-solid border-2 rounded-lg border-slate-950 mb-2">
+                        <input type="text" name="longitude" id="longitude" class="border-solid border-2 rounded-lg border-slate-950 mb-4">
                     </div>
-                    <select name="concernCategory" id="concernCategory" class="py-3">
+                    <select name="concernCategory" id="concernCategory" class="py-3 rounded-lg my-2">
                         <option value="" selected>Choose concern category</option>
                         <option value="Road and Transportation" id="concernCategory">Road and Transportaton</option>
                         <option value="electricity" id="concernCategory">Electricity</option>
@@ -63,23 +61,11 @@ function getRouteContent(route) {
                     </div>
 
                     <div class="py-3">
-                        <label for="image">Enter the image of your concern(optional)</label>
+                        <label for="image">Enter the image of the concern(optional)</label>
                         <input type="file" name="image" id="image" accept="image/png, image/jpeg">
                     </div>
                     <button type="submit" class="bg-emerald-500 rounded-lg p-2 text-base font-semibold text-white">Submit</button>
                 </form>
-            `;
-        case "myTown":
-            return `
-                <h1>Home</h1>
-                <p>Get updates about what is happening in your local area</p>
-                <p>We just need to know your location</p>
-                <form></form>
-            `;
-        default:
-            return `
-                <h1>Home</h1>
-                <p>Welcome to KasiAlot! Explore our services and find out more.</p>
             `;
     }
 }
